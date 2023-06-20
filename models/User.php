3f754260911f6ff20754e26891677fa3deda29b6
anton19ds <bar2018ka@gmail.com>
2023-06-20 13:26:37 +0300

    #env0dist

--- models/User.php
+++ models/User.php
@@ -13,8 +13,8 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     private static $users = [
         '100' => [
             'id' => '100',
-            'username' => $_ENV('ADMIN_LOGIN'),
-            'password' => $_ENV('ADMIN_PASS'),
+            'username' => 'admin',
+            'password' => 'admin',
             'authKey' => 'test100key',
             'accessToken' => '100-token',
         ],
